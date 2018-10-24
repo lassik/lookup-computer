511bbc7376ece04070282cd7681110d969a70a9e
Lassi Kortela <lassi@lassi.io>
2018-10-24 23:19:06 +0300

    Add ASCII slang names

--- ascii.py
+++ ascii.py
@@ -1,11 +1,50 @@
 #! /usr/bin/env python3
 
+import re
+import tarfile
 import unicodedata
 
 import util
 
 
-COLUMNS = ["Binary", "Octal", "Decimal", "Hex", "Display", "Unicode"]
+TARSTEM = "ascii-3.18"
+TARFILE = TARSTEM + ".tar.gz"
+URL = "http://www.catb.org/~esr/ascii/" + TARFILE
+
+COLUMNS = ["Binary", "Octal", "Decimal", "Hex", "Display", "Unicode", "Slang"]
+
+
+def slang_names():
+    slang = [""] * 128
+    field = None
+    i = 0
+    for line in (
+        tarfile.open(util.get_cache_file(TARFILE, URL))
+        .extractfile(TARSTEM + "/nametable")
+        .read()
+        .decode("ascii")
+        .splitlines()
+    ):
+        if line == "%%":
+            i += 1
+            continue
+        m = re.match(r"^([A-Za-z]+):\s*(.*?)\s*$", line)
+        if m:
+            field = m.group(1)
+            value = m.group(2)
+        m = re.match(r"^\s+(.*?)\s*$", line)
+        if m:
+            value = m.group(1)
+        if field == "Synonyms":
+            slang[i] += value
+    for i in range(len(slang)):
+        fields = re.sub(r'["]', " ", slang[i]).split(",")
+        slang[i] = " / ".join(filter(None, map(str.strip, fields)))
+    return slang
+
+
+def slang_name(i):
+    return slang_names()[i]
 
 
 def unicode_name(i):
@@ -33,6 +72,7 @@ def char_row(i):
         "0x{:02x}".format(i),
         display(i),
         unicode_name(i),
+        slang_name(i),
     ]
 
 
